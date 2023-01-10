f0abc10fc3d0d0e6a70f8dfbbd56e1be29829df5
Anatoly <kjluh@mail.ru>
2023-01-10 21:55:28 +0300

    готово

--- src/main/java/com/example/http/HttpApplication.java
+++ src/main/java/com/example/http/HttpApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HttpApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HttpApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HttpApplication.class, args);
+    }
 
 }
